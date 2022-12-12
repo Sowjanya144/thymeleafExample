14ed9df68654fba03cba24f669e8e7ea7b8d97a3
Sowjanya144 <110025804+Sowjanya144@users.noreply.github.com>
2022-12-12 16:29:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
     sudo apt-get update -y 
     sudo apt install default-jdk -y
     sudo apt install maven -y
-    mvn update
-    mvn test
-    mvn package
+    sudo apt-get update maven
+    #mvn test
+    #mvn package
     cd target/
     java -jar work-0.0.1-SNAPSHOT.jar
