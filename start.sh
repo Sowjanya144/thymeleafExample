8f8ba0acc9847d3478cd4ba3d0bef2a4eb0a3227
Sowjanya144 <110025804+Sowjanya144@users.noreply.github.com>
2022-12-12 15:54:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,7 @@
     sudo apt-get update -y 
     sudo apt install default-jdk -y
     sudo apt install maven -y
-    cd /home/azureuser/
-    cd thymeleafExample/
+    cd /home/azureuser/thymeleafExample/
     mvn test
     mvn package
     cd target/
